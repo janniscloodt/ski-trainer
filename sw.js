7b61cf89734f930da77d5c24d19306c9fdd2d041
Jannis Cloodt <j.cloodt@micromata.de>
2026-01-16 09:57:12 +0100

    initial

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "ski-plan-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cached) => cached || fetch(event.request))
+    );
+});
